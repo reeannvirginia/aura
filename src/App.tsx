142b023092d1382f1da4f48a21a9e39f36b3aaf8
Reeann <reeannvirginia@gmail.com>
2020-03-20 14:21:31 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Sidebar, Toggle, Headline } from './components/index';
+import { Homepage, About, Work, Contact } from './views';
+import { pages, links } from './utils/constants';
+import { useStickyTheme } from './utils/custom_hooks';
 
-function App() {
+const App = () => {
+  const [value, setValue] = useStickyTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={`App theme-${value}`}>
+        <div className="wrapper">
+          <Sidebar pages={pages} links={links} />
+          <Toggle className="darkModeToggle" darkMode={value} onChange={setValue} />
+          <Headline />
+          <Switch>
+            <Route path="/" exact component={Homepage} />
+            <Route path="/about" exact component={About} />
+            <Route path="/work" exact component={Work} />
+            <Route path="/contact" exact component={Contact} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
